a78c11f0de14ea43271eaa929785252e6e0bfee9
Kevin Old <kevin@kevinold.com>
2022-07-14 12:25:02 -0500

    amplify push api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nextjsawsamplifyappv": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
---
+++ src/graphql/queries.js
@@ -0,0 +1,89 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      name
+      createdAt
+      completed
+      todoList {
+        id
+        createdAt
+        todos {
+          nextToken
+        }
+        updatedAt
+      }
+      userId
+      updatedAt
+      todoListTodosId
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        createdAt
+        completed
+        todoList {
+          id
+          createdAt
+          updatedAt
+        }
+        userId
+        updatedAt
+        todoListTodosId
+      }
+      nextToken
+    }
+  }
+`;
+export const getTodoList = /* GraphQL */ `
+  query GetTodoList($id: ID!) {
+    getTodoList(id: $id) {
+      id
+      createdAt
+      todos {
+        items {
+          id
+          name
+          createdAt
+          completed
+          userId
+          updatedAt
+          todoListTodosId
+        }
+        nextToken
+      }
+      updatedAt
+    }
+  }
+`;
+export const listTodoLists = /* GraphQL */ `
+  query ListTodoLists(
+    $filter: ModelTodoListFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodoLists(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        createdAt
+        todos {
+          nextToken
+        }
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
